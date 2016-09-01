bb83177644a29123330264287132de18584cf867
Darya <darya.shem@gmail.com>
2016-09-02 00:42:22 +0300

    add require.js to client

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use(bodyParser.json())
 
 app.engine('html', templating.swig);
 app.set('view engine', 'html');
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, "views"));
+app.use(express.static('public'));
 
 
 app.get('/', function(req, res){
