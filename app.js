a8b820970fe54228a92dfe5ad8f1fc59294a9e06
Darya Shemyakina <DShemyakina@domain.corp>
2017-04-04 18:47:13 +0300

    move methods to controller, little styling

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var request = require('request');
 var urlutils = require('url');
 var templating = require('consolidate');
 var path = require("path");
-var db = require("./repositories/db");
 
+var db = require("./repositories/db");
+var wordsController = require("./controllers/wordsController")(db);
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
@@ -16,108 +17,13 @@ app.set('views', path.join(__dirname, "views"));
 app.use(express.static('public'));
 
 
-app.get('/', function(req, res){
-	res.render('index', {
-		title: 'Input word for translate'
-	});
-});
-
-app.post('/', function(req, res){
-	if (!req.body.text || req.body.text == "") {
-		res.json( {
-			title: "Input word for translate!"
-		});
-	} else {
-		var url = urlutils.format({
-			protocol: 'https',
-			hostname: 'translate.yandex.net',
-			pathname: 'api/v1.5/tr.json/translate',
-			query: {
-				key: 'trnsl.1.1.20140416T130443Z.49db75a946e5d9df.baa803157e4482838c0612cb9c5aa513643049a4',
-				lang: req.body.lang,
-				text: req.body.text
-			}
-		});
-
-		request.get({
-				url: url,
-				json: true
-			}, function (error, response, json) {
-				var data = {};
-
-				if (error || json.code != 200) {
-					data = {
-						title: "Error on word translate",
-						error: json && json.message || error
-					}
-				} else {
-					data = {
-						title: 'Translate word ' + req.body.text + ": " + json.text,
-						translate: json.text
-					}
-				}
-				res.json(data);
-			}
-		);
-	}
-});
-
-app.post("/saveWordsPair", function(req, res){
-	return db.getByWords(req.body.cs_word, req.body.ru_word)
-		.then(result => {
-			console.log(result, result.length);
-			if (result && result.length){
-				throw new Error("That words pair already exist");
-			}
-			return db.save({cs_word: req.body.cs_word, ru_word: req.body.ru_word})	
-		})
-		.then(()=>{
-			res.send(200);
-		})
-		.catch((err)=>{
-			res.json({error: err.message});
-		})
-})
-
-app.get("/list", function(req, res){
-	return db.getAll()
-		.then((list)=>{
-			res.render( "list", {items: list});
-		})
-		.catch((err)=>{
-			res.json({error: err.message});
-		})
-})
-
-app.post("/delete", function(req, res){
-	return db.destroy(req.body.id)
-		.then(()=> {
-			res.send(200)
-		})
-		.catch((err)=>{
-			res.json({error: err.message});
-		})
-})
-
-app.get("/training", function(req, res){
-	return db.getUnstudied(20)
-		.then((list)=>{
-			res.render( "training", {items: list});
-		})
-		.catch((err)=>{
-			res.json({error: err.message});
-		})
-});
-
-app.post("/updateRate", function(req, res){
-	return db.updateRate(req.body.resultList)
-		.then(()=> {
-			res.send(200)
-		})
-		.catch((err)=>{
-			res.json({error: err.message});
-		});
-})
+app.get('/', wordsController.index);
+app.post('/', wordsController.translateWord);
+app.post("/saveWordsPair", wordsController.saveWordsPair});
+app.get("/list", wordsController.getList);
+app.post("/delete", wordsController.delete);
+app.get("/training", wordsController.getUnstudiedList);
+app.post("/updateRate", wordsController.updateRate);
 
 app.listen(8080);
 console.log('Express server listening on port 8080');
--- config.js
+++ config.js
@@ -4,6 +4,17 @@ var config = {
 		host: "127.0.0.1",
 		user: "root",
 		password: ""
+	},
+
+	translators: {
+		yandex: {
+			protocol: 'https',
+			hostname: 'translate.yandex.net',
+			pathname: 'api/v1.5/tr.json/translate',
+			query: {
+				key: 'trnsl.1.1.20140416T130443Z.49db75a946e5d9df.baa803157e4482838c0612cb9c5aa513643049a4',
+			}
+		}
 	}
 };
 
